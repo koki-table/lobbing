e10122f319937ffe46e025e205060aedb640af9b
Koki shino <koki.s0327@icloud.com>
2022-04-10 10:11:54 +0900

    ページ速度改善

--- assets/scripts/modules/_Question.js
+++ assets/scripts/modules/_Question.js
@@ -5,14 +5,13 @@ export default class Question {
     this.close = document.querySelectorAll('[data-question-close]')
     this.trigger = document.querySelectorAll('[data-question-trigger]')
     this.target = document.querySelectorAll('[data-question-target]')
-    // this.close = document.querySelector('[data-question-close]')
 
     // questionとanswerの画面切り替えのため、値取得
     this.containerQuestion = document.querySelector('[data-question-container]')
-    this.containerAnswer = document.querySelector('[data-answer-container]')
+    this.secAnswer = document.querySelector('[data-sec-answer]')
+    this.Answer = document.querySelector('[data-answer]')
     this.bodyAnswer = document.querySelector('[data-answer-body]')
     this.bodyAnswerText = document.querySelector('[data-answer-text-target]')
-    // this.appeal = document.querySelector('[data-appeal]')
     this.btn = document.getElementsByClassName('btn')
     this.count = 0
 
@@ -32,32 +31,21 @@ export default class Question {
     _.forEach(this.trigger, el => {
       el.addEventListener('click', this.onClick.bind(this))
     })
-
-    // this.close.addEventListener('click', this.onClose.bind(this))
   }
 
   onClick(e) {
     const config = {
       activeClass: 'is-active'
     }
-    // this.close.classList.remove(config.activeClass)
     this.close.forEach(function(e) {
       e.classList.remove(config.activeClass)
     })
 
-    // const trigger = document.querySelector(`[data-question-trigger="${index}"]`)
-    // trigger.classList.remove(config.activeClass)
-
     // selectボタンを押した回数を監視して、条件分岐
     for (let i = 0; i < this.btn.length; i++) {
-      // if (this.btn[i].checked) {
       this.count++
-      // }
-      // console.log(i)
       console.log(this.count)
       if (this.count === 1) {
-        // this.pagenation02.classList.add(config.activeClass)
-        // console.log(this.pagenation02)
         setTimeout(function(e) {
           const pagenation02 = document.querySelector('[data-pagenation02]')
           pagenation02.classList.add(config.activeClass)
@@ -74,7 +62,8 @@ export default class Question {
         break
       } else if (this.count === 5) {
         this.containerQuestion.classList.remove(config.activeClass)
-        this.containerAnswer.classList.add(config.activeClass)
+        this.secAnswer.classList.add(config.activeClass)
+        this.Answer.classList.add(config.activeClass)
         const index = e.currentTarget.getAttribute('data-question-trigger')
         const target = document.querySelector(
           `[data-answer-text-target="${index}"]`
@@ -84,12 +73,6 @@ export default class Question {
           const bodyAnswer = document.querySelector(`[data-answer-body]`)
           bodyAnswer.classList.add(config.activeClass)
         }, 100)
-        // this.bodyAnswer.classList.add(config.activeClass)
-        // this.appeal.classList.add(config.activeClass)
-        // error.forEach(function(elem) {
-        //   elem.classList.add('active')
-        //   elem.innerHTML = 'あと1問'
-        // })
       }
     }
     const index = e.currentTarget.getAttribute('data-question-trigger')
@@ -97,14 +80,5 @@ export default class Question {
       const target = document.querySelector(`[data-question-target="${index}"]`)
       target.classList.add(config.activeClass)
     }, 100)
-    // document.getElementsByClassName('loading-anime')[0].classList.add('end')
   }
-
-  //   onClose() {
-  //     this.close.classList.remove(config.activeClass)
-
-  //     _.forEach(this.target, el => {
-  //       el.classList.remove(config.activeClass)
-  //     })
-  //   }
 }
